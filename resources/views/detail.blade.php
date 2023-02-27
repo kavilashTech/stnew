8c78772725e8a01aa3cba7740b8412655c50a85e
Sampath <kavilashtech@gmail.com>
2023-02-27 20:35:56 +0530

    updated availability files

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -106,7 +106,7 @@
                     @endif
                 </div>
             </div>
-        @if(isset($gallery_image[1]) || isset($gallery_image[2]) )
+@if(isset($gallery_image[1]) || isset($gallery_image[2]) )
             <div class="col-md-3">
             @if(isset($gallery_image[1]))
                 <div class="row dd-none">
@@ -197,135 +197,76 @@
                         <div class="col-md-4 col-6 pr-2"><div class="amy"><i class="material-icons mr-1">gite</i><span class="font-medium text-sm text-text ">{{$propertyamentiedata['name']}}</span></div></div>
                         @endforeach
                     </div>
-                    <div class="d-flex" style="position:absolute; bottom:0px">
-                        <h3>Exclusive :  </h3>
-                        <p style="font-size: 16px; line-height: 2rem; font-weight:600;">&nbsp;&nbsp;{{@$rows->exclusivity ? $rows->exclusivity->name : 'N/A' }}</p>
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<div class="tab-section">    
-    <div class="container ">
-        <div class="row">
-            <div class="col-md-12">
-                <ul class="nav nav-tabs  property-detail-tab-section " >
-                    <li class="nav-item" style="    margin: 0px 15px;">
-                        <a class="nav-link" style="position:relative;padding:8px;"  href="#booking-section"><h4>Room Detail</h4></a>
-                    </li>
-                    <li class="nav-item" style="    margin: 0px 15px;">
-                        <a class="nav-link" style="position:relative; padding:8px;"   href="#location-section"><h4>Location</h4></a>
-                    </li>
-                    <li class="nav-item" style="    margin: 0px 15px;">
-                        <a class="nav-link" style="position:relative; padding:8px;"  href="#tab3"><h4>Amenities</h4></a>
-                    </li>
-                    <li class="nav-item" style="    margin: 0px 15px;">
-                        <a class="nav-link" style="position:relative; padding:8px;"  href="#tab4"><h4>Property Video</h4></a>
-                    </li>
-                    <li class="nav-item" style="    margin: 0px 15px;">
-                        <a class="nav-link" style="position:relative; padding:8px;"  href="#policy-section"><h4>Property Policy</h4></a>
-                    </li>
-                    <li class="nav-item" style="    margin: 0px 15px;">
-                        <a class="nav-link" style="position:relative; padding:8px;"  href="#review-section"><h4>Reviews</h4></a>
-                    </li>
-                </ul>              
-            </div>
-        </div>
-    </div>
-</div>
 
 
-<section class="booking-section" id="booking-section" >
-    <div class="container" style="    background-color: #E8F0F2;
-    padding: 14px 15px;">
+
+    <div class="booking-section">
+    <div class="container">
         <div class="row">
             <div class="col-lg-12">
                 <div class="top-sticky">
                     <div class="row v-centter">
                         <div class="col-md-5">
-                            <div class="header-section" >
-                                <h3 class="mb-0" style="text-align:left !important">Book your stay</h3>
+                            <div class="header-section">
+                                <h3 class="mb-0">Book your stay</h3>
                                 <p class="gray">Select from a range of beautiful rooms</p>
                             </div>
                         </div>
                         <div class="col-md-7">
-                            <div class="v-centter justify-content-end">
+                            <!-- <div class="v-centter justify-content-end">
                                 <div class="dropdown dropdown-toggle red-drop mr-4">USD</div>
                                 <div class="calender">
                                     <div class="cal1">Mon 19 Dec, 2022</div>
                                     <div class="pt-2 pl-2 pr-2 gray"><i class="material-icons mr-1">east</i></div>
                                     <div class="cal2">Tue 20 Dec, 2022</div>
                                 </div>
-                            </div> 
+                            </div> -->
                         </div>
                     </div>
                 </div>
                 @if(count($list_of_rooms) > 0 )
+                @foreach($list_of_rooms as $roomdata)
 
 
-                    @foreach($list_of_rooms as $roomdata)
-
-                        <div class="room-section">
-                            <div class="rooms">
-                                <div class="row no-gutters">
-                                    <div class="col-lg-2 col-md-12">
-                                        <div class="room-slider">
-                                            <div class="room-img"><img data-src="{{url('images/room1/slide1.jpg')}}" src="{{url('images/room1/slide1.jpg')}}" class="lazyload img-fluid" alt="image"></div>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-12 col-md-12 roomContainer">
-                                        <div class="room-details">
-                                            <div class="head">
-                                                <div class="">{{$roomdata->name}}<br/><span class="font14 v-centter normal mt-2"><i class="material-icons font16">person</i> X 1</span></div>
-                                                <table class="price-table">
-                                                    <tr><td>Total Beds :</td><td>{{$roomdata->total_bed_count}}</td></tr>
-                                                    <tr><td class="bold">Rent :</td><td class="bold">&#8377; {{$roomdata->price_per_month}} / Month</td></tr>
-
-                                                    <tr><td>Deposit :<br/> @if($roomdata->refundable == 1)<span class="font10">(Refundable)</span> @endif</td><td>&#8377; {{$roomdata->deposit}}</td></tr>
-
-                                                </table>
-                                            </div>
-                                            <div class="row room-facilities">
-                                                <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">local_drink</i><span class="font-medium text-sm text-text ">Water Dispenser</span></div></div>
-                                                <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">ac_unit</i><span class="font-medium text-sm text-text ">Air-conditioning</span></div></div>
-                                                <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">room_service</i><span class="font-medium text-sm text-text ">24/7 Reception</span></div></div>
-                                                <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">vpn_key</i><span class="font-medium text-sm text-text ">Lockers</span></div></div>
-                                                <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">hot_tub</i><span class="font-medium text-sm text-text ">Hot water</span></div></div>
-                                                <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">local_laundry_service</i><span class="font-medium text-sm text-text ">Laundry Services (Extra)</span></div></div>
-                                                <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">wifi</i><span class="font-medium text-sm text-text ">Free Wi-Fi</span></div></div>
-                                                <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">payment</i><span class="font-medium text-sm text-text ">Card Payment Accepted</span></div></div>
-                                                <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">tv</i><span class="font-medium text-sm text-text ">Common Television</span></div></div>
-                                            </div>
-
-                                            <div class="availability">
-                                                <div class="avail one">Availability Calendar <i class="fa fa-angle-down" aria-hidden="true"></i></div>
-                                                <div class="">
-                                                    <input type="number" name=""> <label>Month</label><br/>
-                                                    <a href="javascript:;" class="book-btn">Book Now</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                <div class="room-section">
+                    <div class="rooms ">
+                        <div class="row no-gutters">
+                            <div class="col-lg-2 col-md-12">
+                                <div class="room-slider">
+                                    <div class="room-img"><img data-src="{{url('images/room1/slide1.jpg')}}" src="{{url('images/room1/slide1.jpg')}}" class="lazyload img-fluid" alt="image"></div>
                                 </div>
                             </div>
+                            <div class="col-lg-10 col-md-12 roomContainer">
+                                <div class="room-details">
+                                    <div class="head">
+                                        <div class="">{{$roomdata->name}}<br/><span class="font14 v-centter normal mt-2"><i class="material-icons font16">person</i> X 1</span></div>
+                                        <table class="price-table">
+                                            <tr><td>Total Beds :</td>
+											<td>{{$roomdata->total_bed_count}}</td>
+											</tr>
+                                            <tr><td class="bold">Rent :</td><td class="bold">&#8377; {{$roomdata->price_per_month}} / Month</td></tr>
 
+                                            <tr><td>Deposit :<br/> @if($roomdata->refundable == 1)<span class="font10">(Refundable)</span> @endif</td>
+											<td>&#8377; {{$roomdata->deposit}}</td></tr>
 
-                            <div class="cale-slider">
-                                <div id="pro-carousel1" class="owl-carousel owl-theme">
-                                    <div class="item">
-                                        <div class="price-box">
-                                            <div class="gray font14">MON<br/><span class="font-black">Jan 01</span></div>
-                                            <div class="price">$7.23</div>
-                                            <div class="font12 bold">18 Units</div>
-                                        </div>
+                                        </table>
                                     </div>
-                                    <div class="item">
-                                        <div class="price-box">
-                                            <div class="gray font14">MON<br/><span class="font-black">Jan 02</span></div>
-                                            <div class="price">$7.23</div>
-                                            <div class="font12 bold">18 Units</div>
-                                        </div>
+                                    <div class="row room-facilities">
+                                        <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">local_drink</i><span class="font-medium text-sm text-text ">Water Dispenser</span></div></div>
+                                        <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">ac_unit</i><span class="font-medium text-sm text-text ">Air-conditioning</span></div></div>
+                                        <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">room_service</i><span class="font-medium text-sm text-text ">24/7 Reception</span></div></div>
+                                        <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">vpn_key</i><span class="font-medium text-sm text-text ">Lockers</span></div></div>
+                                        <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">hot_tub</i><span class="font-medium text-sm text-text ">Hot water</span></div></div>
+                                        <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">local_laundry_service</i><span class="font-medium text-sm text-text ">Laundry Services (Extra)</span></div></div>
+                                        <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">wifi</i><span class="font-medium text-sm text-text ">Free Wi-Fi</span></div></div>
+                                        <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">payment</i><span class="font-medium text-sm text-text ">Card Payment Accepted</span></div></div>
+                                        <div class="col-lg-4 col-md-4 col-sm-6"><div class="amy"><i class="material-icons mr-1">tv</i><span class="font-medium text-sm text-text ">Common Television</span></div></div>
                                     </div>
 
                                     <div class="availability">
@@ -341,6 +282,7 @@
                                 <div class="row">
                                                 <div class="col-12 roomAvailabilityCalendar"></div>
                                             </div>
+
                             </div>
                         </div>
                     </div>
@@ -363,11 +305,14 @@
                 @endif
 
                
+            </div>
+        </div>
+    </div>
+</div>
 
 
- <section class="location-section" id="location-section">
-    <div class="container" style="    background: white;
-    padding: 15px;">
+<div class="location-section">
+    <div class="container">
         <div class="header-section">
             <h3>Locate Us</h3>
         </div>
@@ -401,83 +346,24 @@
                 <iframe title="google-maps-parnter-location" width="100%" height="250" loading="lazy" class="w-full h-64 z-0 rounded-lg sm:mt-0" src="https://www.google.com/maps/embed/v1/place?q=12.964846750305556,77.64206038979783&amp;zoom=12&amp;key=AIzaSyC0VzzsqCWpoAd6YYwhwl_lxX1_GIIaSA8"></iframe>
             </div>
         </div>
-    </div>
-</section> 
-<div class="review-section" id="review-section">
-    <div class="container">
-        <div class="header-section">
-            <h3>Reviews</h3>
-        </div>
-        @if(count($rows->rating) > 0)
-            @foreach ($rows->rating as $item )
-          
-                <div class="row" style="margin: 20px 0px;
-                border-bottom: 1px solid #ff5a5f; padding-bottom: 12px;">
-                    <div class="col-md-2 d-flex">                                    
-                        <div>
-                            @if($item->user->profile_img)
-                                <img src="{{asset('images/users/'.$item->user->profile_img)}}" class="review-img">
-                            @else
-                                <img src="{{asset('images/users/default.jpg')}}" class="review-img">
-                            @endif
-                        </div>
-                        <div class="property-review" style="padding: 10px 10px;">
-                            <p>{{$item->user->display_name}}</p>
-                            <small class="review-star">
-                                @for ($i=1; $i <= $item->rating ; $i++)
-                                    <i class="fa fa-star text-thm" aria-hidden="true"></i>
-                                @endfor
-                                @for ($i=1; $i <= (5 - $item->rating) ;$i++)
-                                    <i class="fa fa-star-o text-thm" aria-hidden="true"></i>
-                                @endfor
-                            </small>
-                            {{-- <small class="ml-2">12 views</small> --}}
-                        </div>                                    
-                    </div>      
-                    <div class="col-md-10"> {!! $item->reviewcomments !!} 
-                        @if($item->owner_id != null && $item->owner_reply != null)                              
-                            {{-- <div class="row"> --}}
-                            <div class="d-flex justify-content-start">
-                                @if($item->owner->profile_img)
-                                    <img src="{{asset('images/users/'.$item->owner->profile_img)}}" style=" width: 90px;
-                                    border-radius: 10px;
-                                    height: 90px;">
-                                                @else
-                                    <img src="{{asset('images/users/default.jpg')}}" style="width: 90px;
-                                    border-radius: 10px;
-                                    height: 90px;">
-                                @endif
-                                <div class="px-4">
-                                    <h5>{{$item->owner->display_name}}</h5>
-                                    {!! $item->owner_reply  !!}
-                                </div>
-                            </div>
-                        @endif                        
-                    </div>                                    
-                </div>
-            @endforeach
-        @else
-            <div class="row">
-                <div class="col-md-12">
-                    <h4> No Record Found! </h4>
-                </div>  
-            </div>
-        @endif   
+
     </div>
 </div>
-<div class="policy-section" id="policy-section">
+
+<div class="policy-section">
     <div class="container">
-        <div class="header-section">
-            <h3>Property Policy</h3>
-        </div>
+    <div class="header-section">
+        <h3>Property Policy</h3>
+    </div>
         <div class="mt-3">
-            {!! $rows->policy !!}
+        {!! $rows->policy !!}
+
         </div>
     </div>
-</div> 
+</div>
 
 
-<div class="cancellation" id="cencellation">
+<div class="cancellation">
     <div class="container">
         <div class="header-section">
             <h3>Cancellation Policy</h3>
@@ -498,6 +384,9 @@
     </div>
 </div>
 
+
+
+@endsection
 
 
 @section('script')
@@ -839,8 +728,6 @@
         }
     });
 </script>
-
-@endsection
 @endsection
 
 
