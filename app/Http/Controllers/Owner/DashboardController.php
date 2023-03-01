5e77ae01e9707c260d8a35509698c371c8f6fb58
DEV Pallavi <pallavisharma9991@gmail.com>
2023-03-01 21:33:19 +0530

    minor changes

--- app/Http/Controllers/Owner/DashboardController.php
+++ app/Http/Controllers/Owner/DashboardController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Hash;
 class DashboardController extends Controller
 {
     public function index(){
-        return view('owner.Dashboard');
+    // dd(Hash::make('123456'));
+        return view('owner.dashboard');
     }
 }
