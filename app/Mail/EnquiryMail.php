2db174aa50188311d252fe72242d13acc2fd139b
DEV Pallavi <pallavisharma9991@gmail.com>
2023-03-11 14:17:10 +0530

    minor changes

--- app/Mail/EnquiryMail.php
+++ app/Mail/EnquiryMail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class EnquirtMail extends Mailable
+class EnquiryMail extends Mailable
 {
     use Queueable, SerializesModels;
 
